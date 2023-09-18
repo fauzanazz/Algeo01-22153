676908d1e0547c535b0cb19eb2c5360f16cd635d
ValentinoTriadi <valent122016@gmail.com>
2023-09-18 16:40:59 +0700

    feat(determinan): Add determinan 2x2 3x3

--- Input File/cek/TextToMatriks.java
+++ Input File/cek/TextToMatriks.java
@@ -34,34 +34,20 @@ public class TextToMatriks {
         return matrix;
     }
 
-    public static void printMatrix(float[][] matrix){
-        int i,j;
-        for (i=0; i<matrix.length; i++) {
-            for (j=0; j<matrix[i].length; j++){
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+    // public static void printMatrix(float[][] matrix){
+    //     int i,j;
+    //     for (i=0; i<matrix.length; i++) {
+    //         for (j=0; j<matrix[i].length; j++){
+    //             System.out.print(matrix[i][j] + " ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
 
-    // public static void main(String[] args) {
-
-    //     String text = "1 2 3 4\n5 6 7 8\n9 10 11 12\n13 14 15 16";
-	// 	// float[][] matrix = { { 3, 2, 1, 7 },
-	// 	// 			 { 9, 11, 5, 4 },
-	// 	// 			 { 6, 0, 13, 17 },
-	// 	// 			 { 7, 21, 14, 15 } };
-	// 	// printMatrix(matrix, 4, 4);
-    //     float[][] matriks = getMatrixFromText(text);
-	// 	printMatrix(matriks, 4, 4);
-	// }
-
-
-
-    public static float[][] readMatrixFromFile() {
+    public float[][] readMatrixFromFile(String path) {
         String text = "";
         try {
-            File f = new File("filename.txt");
+            File f = new File(path);
             Scanner line = new Scanner(f);
             while (line.hasNextLine()) {
                 text = text + line.nextLine() + "\n";
