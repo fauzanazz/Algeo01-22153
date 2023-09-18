5405d78fec975e249224157d36b0f0f74bde696c
ValentinoTriadi <valent122016@gmail.com>
2023-09-18 10:31:08 +0700

    Update(input): Update read matrix from file

--- Input File/TextToMatriks.java
+++ Input File/TextToMatriks.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class TextToMatriks {
-    public static int[][] getMatrixFromText(String text){
+    private static float[][] getMatrixFromText(String text){
         // Get row & column
         int m = 0, n = 1;
         for (char x: text.toCharArray()) {
@@ -12,7 +12,7 @@ public class TextToMatriks {
         }
         
         // Create Empty Matrix & initiate variable
-        int[][] matrix = new int[m][n];
+        float[][] matrix = new float[m][n];
         int i = 0, j = 0;
         String temp = "";
 
@@ -21,7 +21,7 @@ public class TextToMatriks {
             if (x != ' ' && x != '\n') {
                 temp = temp + String.valueOf(x);
             } else {
-                matrix[i][j] = Integer.parseInt(temp);
+                matrix[i][j] = Float.parseFloat(temp);
                 j++;
                 temp = "";
                 if (j == n) {
@@ -34,10 +34,10 @@ public class TextToMatriks {
         return matrix;
     }
 
-    public static void printMatrix(int[][] matrix, int m, int n){
+    public static void printMatrix(float[][] matrix){
         int i,j;
-        for (i=0; i<m; i++) {
-            for (j=0; j<n; j++){
+        for (i=0; i<matrix.length; i++) {
+            for (j=0; j<matrix[i].length; j++){
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
@@ -47,18 +47,18 @@ public class TextToMatriks {
     // public static void main(String[] args) {
 
     //     String text = "1 2 3 4\n5 6 7 8\n9 10 11 12\n13 14 15 16";
-	// 	// int[][] matrix = { { 3, 2, 1, 7 },
+	// 	// float[][] matrix = { { 3, 2, 1, 7 },
 	// 	// 			 { 9, 11, 5, 4 },
 	// 	// 			 { 6, 0, 13, 17 },
 	// 	// 			 { 7, 21, 14, 15 } };
 	// 	// printMatrix(matrix, 4, 4);
-    //     int[][] matriks = getMatrixFromText(text);
+    //     float[][] matriks = getMatrixFromText(text);
 	// 	printMatrix(matriks, 4, 4);
 	// }
 
 
 
-    public static void main(String[] args) {
+    public static float[][] readMatrixFromFile() {
         String text = "";
         try {
             File f = new File("filename.txt");
@@ -71,7 +71,6 @@ public class TextToMatriks {
             System.out.println("File Not Found!");
         }
 
-        int[][] matriks = getMatrixFromText(text);
-        printMatrix(matriks, 4, 4);
+        return getMatrixFromText(text);
     }
 }
